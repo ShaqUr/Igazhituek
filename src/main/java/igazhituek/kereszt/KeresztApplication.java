3dc1e57067914cf3e10c5fddaf67616d568a837b
ShaqUr <shakkouraram@gmail.com>
2018-03-14 22:39:22 +0100

    elso verzio

---
+++ src/main/java/igazhituek/kereszt/KeresztApplication.java
@@ -0,0 +1,12 @@
+package igazhituek.kereszt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeresztApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeresztApplication.class, args);
+	}
+}
