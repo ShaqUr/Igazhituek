78276e07aaf5797e7e3feddc23d171a2a8e49345
ShaqUr <shakkouraram@gmail.com>
2018-03-22 18:08:25 +0100

    lacziknak pelda

--- src/main/java/igazhituek/kereszt/KeresztApplication.java
+++ src/main/java/igazhituek/kereszt/KeresztApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KeresztApplication {
-
+        
 	public static void main(String[] args) {
+                //szia laczik!
 		SpringApplication.run(KeresztApplication.class, args);
 	}
 }
