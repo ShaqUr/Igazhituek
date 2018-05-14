009a3738a8a112f6350c78b0334f01ce579942de
tomla <tomla@192.168.0.123>
2018-05-14 20:24:30 +0200

    Cookie kezelés

--- src/main/resources/static/script/login_check.js
+++ src/main/resources/static/script/login_check.js
@@ -2,9 +2,17 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
 	if(this.readyState == 4 && this.status == 200) {
 		if(this.responseText === 'false') {
+                        alert('isloggedin falset valaszolt rohadna ki a bele');
 			document.location = 'index.html';
 		}
 	}
 };
-xhttp.open("GET", "http://localhost:8080/api/user/isloggedin", true);
+alert('elkuldtem isloggedinnek: ' + getCookie('userid'));
+xhttp.open("GET", "http://localhost:8080/api/user/isloggedin?id=" + getCookie('userid'), true);
 xhttp.send();
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
