45914eaaac88b18941fa7b637d03a8d2f15ea4c3
tomla <tomla@192.168.0.123>
2018-05-19 13:33:35 +0200

    Logout fix

--- src/main/resources/static/script/login_check.js
+++ src/main/resources/static/script/login_check.js
@@ -1,3 +1,16 @@
 if(getCookie('userid') === undefined) {
     document.location = 'index.html';
 }
+
+window.addEventListener('load', function() {
+    document.getElementById('logout_btn').onclick = function() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                    document.location = 'index.html';
+            }
+        };
+        xhttp.open("GET", "/api/user/logout?id=" + getCookie(userid), true);
+        xhttp.send();
+    };
+});
