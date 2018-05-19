6d362a600f897e2978b60f0b0bb427d81f4d8fe1
tomla <tomla@192.168.0.123>
2018-05-19 12:27:34 +0200

    Login fix

--- src/main/resources/static/script/login_check.js
+++ src/main/resources/static/script/login_check.js
@@ -1,9 +1,3 @@
 if(getCookie('userid') === undefined) {
     document.location = 'index.html';
 }
-
-function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
-}
