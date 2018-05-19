1502171b17de6b830d47558c4571065153d79845
tomla <tomla@192.168.0.123>
2018-05-19 14:10:57 +0200

    Logout fix

--- src/main/resources/static/script/login_check.js
+++ src/main/resources/static/script/login_check.js
@@ -4,14 +4,7 @@ if(getCookie('userid') === undefined) {
 
 window.addEventListener('load', function() {
     document.getElementById('logout_btn').onclick = function() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if(this.readyState == 4 && this.status == 200) {
-                document.cookie = 'userid=;Path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-                document.location = 'index.html';
-            }
-        };
-        xhttp.open("GET", "/api/user/logout?id=" + getCookie('userid'), true);
-        xhttp.send();
+        document.cookie = 'userid=;Path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        document.location = 'index.html';
     };
 });
