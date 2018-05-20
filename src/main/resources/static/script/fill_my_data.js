3904e729532c42e959940f76a030803dbc2e7758
tomla <tomla@192.168.0.123>
2018-05-20 14:41:04 +0200

    Profilképek méretezése, my data javitasa

--- src/main/resources/static/script/fill_my_data.js
+++ src/main/resources/static/script/fill_my_data.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', function() {
     var username = getCookie('username');
-    document.querySelector('#my_data img').src = getCookie('picture');
-    document.querySelector('#my_data h1').innerHTML = username;
+    document.querySelector('#my_name').innerHTML = username;
 });
